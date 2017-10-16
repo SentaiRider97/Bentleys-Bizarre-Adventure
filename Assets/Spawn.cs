e67c154cc50f52435203f6dbc2bb942cf5bd5cc5
Unknown <cl3884@nyu.edu>
2017-10-15 23:29:23 -0400

    Victory and Game Over

--- Assets/Spawn.cs
+++ Assets/Spawn.cs
@@ -6,43 +6,102 @@ using UnityEngine.UI;
 public class Spawn : MonoBehaviour {
 	public Transform hydrant; 
 	public Transform bush;
-	public Transform poop; 
+	public Transform house1; 
 	public Transform dog;   
 	public Transform shit; 
+	public Transform house2;
+	public Transform house3;
+	public Transform house4;
+	public Transform house5;
+	public Transform house6;
+	public Transform house7;
+	public Transform house8;
+	public Transform house9;
+	public Transform otherhuman;
 	Vector3 dogPos; 
 	bool hydrantchecker;
 	bool bushchecker; 
-	bool poopchecker;
+	bool house1checker;
+	bool house2checker;
+	bool house3checker;
+	bool house4checker;
+	bool house5checker;
+	bool house6checker;
+	bool house7checker;
+	bool house8checker;
+	bool house9checker;
+	bool otherhumanchecker;
 	public Text myText; 
 	public int Counter=0; 
+	public Canvas victoryCanvas; 
+
+	void Start ()
+	{
+		victoryCanvas.enabled = false; 	
+	}
 	
 	// Update is called once per frame
 	void Update () {
 		myText.text = "Poop Collector: " + Counter.ToString ();
 		dogPos = dog.transform.position;  
-		if (Vector3.Distance (dog.transform.position, hydrant.transform.position) < 3f && hydrantchecker == false) {
+//		Debug.Log (Vector3.Distance (dog.transform.position, hydrant.transform.position)); 
+		if (Vector3.Distance (dog.transform.position, hydrant.transform.position) < 8f && hydrantchecker == false) {
 			Instantiate (shit, dogPos, Quaternion.identity);
 			hydrantchecker = true;
+			//Debug.Log ("hydrant");
 		}  
 
-		if (Vector3.Distance (dog.transform.position, bush.transform.position) < 15f  && bushchecker == false) {
+		if (Vector3.Distance (dog.transform.position, bush.transform.position) < 10f && bushchecker == false) {
 			Instantiate (shit, dogPos, Quaternion.identity);
 			bushchecker = true;
+			//Debug.Log ("bush");
 		} 
 
-		if (Vector3.Distance (dog.transform.position, poop.transform.position) < 5f && poopchecker == false) {
+		if (Vector3.Distance (dog.transform.position, house1.transform.position) < 20f && house1checker == false) {
 			Instantiate (shit, dogPos, Quaternion.identity);
-			poopchecker = true;
+			house1checker = true;
+			//Debug.Log ("poop");
 		}
-			
-		if (Vector3.Distance (dog.transform.position, hydrant.transform.position) > 3f && hydrantchecker == true) {
-			hydrantchecker = false;
+		if (Vector3.Distance (dog.transform.position, house2.transform.position) < 20f && house2checker == false) {
+			Instantiate (shit, dogPos, Quaternion.identity);
+			house2checker = true;
 		}
-		if (Vector3.Distance (dog.transform.position, bush.transform.position) > 15f && bushchecker == true) {
-			bushchecker = false;
+		if (Vector3.Distance (dog.transform.position, house3.transform.position) < 20f && house3checker == false) {
+			Instantiate (shit, dogPos, Quaternion.identity);
+			house3checker = true;
 		}
-		if (Vector3.Distance (dog.transform.position, poop.transform.position) > 5f && poopchecker == true) {
-			poopchecker = false;
+		if (Vector3.Distance (dog.transform.position, house4.transform.position) < 20f && house4checker == false) {
+			Instantiate (shit, dogPos, Quaternion.identity);
+			house4checker = true;
+		}
+		if (Vector3.Distance (dog.transform.position, house5.transform.position) < 20f && house5checker == false) {
+			Instantiate (shit, dogPos, Quaternion.identity);
+			house5checker = true;
+		}
+		if (Vector3.Distance (dog.transform.position, house6.transform.position) < 20f && house6checker == false) {
+			Instantiate (shit, dogPos, Quaternion.identity);
+			house6checker = true;
+		}
+		if (Vector3.Distance (dog.transform.position, house7.transform.position) < 20f && house7checker == false) {
+			Instantiate (shit, dogPos, Quaternion.identity);
+			house7checker = true;
+		}
+		if (Vector3.Distance (dog.transform.position, house8.transform.position) < 20f && house8checker == false) {
+			Instantiate (shit, dogPos, Quaternion.identity);
+			house8checker = true;
+		}
+		if (Vector3.Distance (dog.transform.position, house9.transform.position) < 20f && house9checker == false) {
+			Instantiate (shit, dogPos, Quaternion.identity);
+			house9checker = true;
+		}
+		if (Vector3.Distance (dog.transform.position, otherhuman.transform.position) < 10f && otherhumanchecker == false) {
+			Instantiate (shit, dogPos, Quaternion.identity);
+			otherhumanchecker = true;
+		}
+		if(Counter==10)
+		{
+			victoryCanvas.enabled = true; 
+			Time.timeScale = 0f; 
 		}
 	}
 
---
+++ Assets/TimeLimit.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TimeLimit : MonoBehaviour {
+
+	public Canvas gameoverCanvas;
+	float timelimit = 120f;
+	public Text secondsText;
+	public Text minutesText;
+	float seconds = 59.9998f;
+	int minutes = 1;
+
+	void Start() {
+		gameoverCanvas.enabled = false;
+	}
+
+	void Update() {
+		timelimit -= Time.deltaTime;
+
+		if (timelimit <= 0.001f) {
+			Time.timeScale = 0f;
+			gameoverCanvas.enabled = true;
+		}
+
+		seconds -= Time.deltaTime;
+		if (seconds < 9.5f) {
+			secondsText.text = "0" + (Mathf.Round (seconds)).ToString ();
+		} else { secondsText.text = (Mathf.Round (seconds)).ToString (); }
+		if (seconds <= 0.000001f) {
+			seconds = 59f;
+			minutes--;
+		}
+
+		if (minutes > 0) {
+			minutesText.text = (minutes.ToString ()) + ":";
+		}
+		else { minutesText.text = "0:";}
+	}
+}
